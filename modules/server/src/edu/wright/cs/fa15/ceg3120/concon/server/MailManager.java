e3cc4701db068ee35969bad078f0cf076fd7ee06
Nathan Jent <nathanjent@gmail.com>
2015-09-18 01:36:38 -0400

    Added classes to handle notification email to users. Example code needs edited for use in production Added chat message handler class. Added Apache commons-email library.

---
+++ modules/server/src/edu/wright/cs/fa15/ceg3120/concon/server/MailManager.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2015
+ * 
+ * 
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.wright.cs.fa15.ceg3120.concon.server;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Handles email sent by the server to notify users of status changes
+ * and mail messages from other users.
+ * @author NathanJent
+ *
+ */
+public class MailManager {
+
+	public void SendEmail() {
+		// TODO convert this example code into a usable class
+		
+		// Recipient's email ID needs to be mentioned.
+		String to = "abcd@gmail.com";
+
+		// Sender's email ID needs to be mentioned
+		String from = "web@gmail.com";
+
+		// Assuming you are sending email from localhost
+		String host = "localhost";
+
+		// Get system properties
+		Properties properties = System.getProperties();
+
+		// Setup mail server
+		properties.setProperty("mail.smtp.host", host);
+
+		// Get the default Session object.
+		Session session = Session.getDefaultInstance(properties);
+
+		try {
+			// Create a default MimeMessage object.
+			MimeMessage message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			// Set Subject: header field
+			message.setSubject("This is the Subject Line!");
+
+			// Now set the actual message
+			message.setText("This is actual message");
+
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+}
