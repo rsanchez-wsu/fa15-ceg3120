bb71c1637def1de428fc88ee2cdff795578a7383
pquackenbush <quackenbush.2@wright.edu>
2015-09-24 12:52:48 -0400

    [#9] added data fields to HomeownerAccount

--- common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/HomeOwnerAccount.java
+++ common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/HomeOwnerAccount.java
@@ -24,6 +24,11 @@ package edu.wright.cs.fa15.ceg3120.concon.common.data;
 public class HomeOwnerAccount extends UserAccount {
 
     private String streetAddress;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phoneNumber;
+    private String email;
     //other fields here
     
     public HomeOwnerAccount() {
@@ -43,6 +48,46 @@ public class HomeOwnerAccount extends UserAccount {
 
 	public void setStreetAddress(String streetAdress) {
 		this.streetAddress = streetAdress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
