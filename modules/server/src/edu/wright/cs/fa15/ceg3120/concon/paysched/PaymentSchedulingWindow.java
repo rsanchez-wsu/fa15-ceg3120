aa941e42de6931408663fbc9b65764220afc4b99
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2015-09-30 17:15:19 -0400

    Just to see what happens.
    No need to panic, but I may have accidentally broke the repo.

--- modules/server/src/edu/wright/cs/fa15/ceg3120/concon/paysched/PaymentSchedulingWindow.java
+++ modules/server/src/edu/wright/cs/fa15/ceg3120/concon/paysched/PaymentSchedulingWindow.java
@@ -1,30 +1,49 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015
+ * 
+ * 
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 package edu.wright.cs.fa15.ceg3120.concon.paysched;
 
-import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
+import javax.swing.JFrame;
+
 /**
+ * Window to display payment and scheduling information.
  *
  * @author Emily
  */
 public class PaymentSchedulingWindow {
 
     /**
+     * Main entry point.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         JFrame paymentAndScheduling = new JFrame("Payment and Scheduling");
-     paymentAndScheduling.setDefaultCloseOperation(EXIT_ON_CLOSE);
-     paymentAndScheduling.setSize(475, 300);
-     TabbedFrame grid = new TabbedFrame();
-     grid.setVisible(true);
-     paymentAndScheduling.add(grid);
-     paymentAndScheduling.setVisible(true);
+        paymentAndScheduling.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        paymentAndScheduling.setSize(475, 300);
+        TabbedFrame grid = new TabbedFrame();
+        grid.setVisible(true);
+        paymentAndScheduling.add(grid);
+        paymentAndScheduling.setVisible(true);
     }
     
 }
