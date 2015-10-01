4679594cbf40f2c035d10d734618b2189141d553
jsthomas45320 <thomas.611@wright.edu>
2015-10-01 18:12:08 -0400

    Updated Edit Profile tab

--- modules/contractor_client/src/edu/wright/cs/fa15/ceg3120/concon/client/contractor/ContractorClient.java
+++ modules/contractor_client/src/edu/wright/cs/fa15/ceg3120/concon/client/contractor/ContractorClient.java
@@ -24,16 +24,50 @@ package edu.wright.cs.fa15.ceg3120.concon.client.contractor;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.JSplitPane;
+import javax.swing.JEditorPane;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
 
 public class ContractorClient {
 
 	private JFrame frame;
+	private JTextField txtLastName;
+	private JTextField txtFirstName;
+	private JTextField txtCompanyName;
+	private JTextField txtAddress1;
+	private JTextField txtAddress2;
+	private JTextField txtCity;
+	private JTextField txtState;
+	private JTextField txtZipCode;
+	private JTextField txtLastNameUpdate;
+	private JTextField txtFirstNameUpdate;
+	private JTextField txtCompanyNameUpdate;
+	private JTextField txtAddress1Update;
+	private JTextField txtAddress2Update;
+	private JTextField txtCityUpdate;
+	private JTextField txtStateUpdate;
+	private JTextField txtZipCodeUpdate;
+	public String strLastName = "Person";
+	public String strFirstName = "Random";
+	public String strCompanyName = "ConCon";
+	public String strAddress1 = "123 Main Street";
+	public String strAddress2 = "";
+	public String strCity = "Dayton";
+	public String strState = "OH";
+	public int intZipCode = 45402;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -44,6 +78,7 @@ public class ContractorClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
 			}
 		});
 	}
@@ -52,6 +87,7 @@ public class ContractorClient {
 	 * Create the application.
 	 */
 	public ContractorClient() {
+
 		initialize();
 	}
 
@@ -136,11 +172,195 @@ public class ContractorClient {
 		
 		JPanel profileTab = new JPanel();
 		pageTabs.addTab("Edit Profile", null, profileTab, null);
+		profileTab.setLayout(null);
+		
+		JLabel lblCurProfile = new JLabel("Current Profile Settings:");
+		lblCurProfile.setBounds(30, 5, 200, 20);
+		profileTab.add(lblCurProfile);
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		lblLastName.setBounds(15, 35, 120, 20);
+		profileTab.add(lblLastName);
+		
+		txtLastName = new JTextField();
+		txtLastName.setBounds(145, 35, 120, 20);
+		profileTab.add(txtLastName);
+		txtLastName.setColumns(1);
+		txtLastName.setText(strLastName);
+		
+		JLabel lblFirstName = new JLabel("First Name:");
+		lblFirstName.setBounds(15, 60, 120, 20);
+		profileTab.add(lblFirstName);
+		
+		txtFirstName = new JTextField();
+		txtFirstName.setBounds(145, 60, 120, 20);
+		profileTab.add(txtFirstName);
+		txtFirstName.setColumns(1);
+		txtFirstName.setText(strFirstName);
+		
+		JLabel lblCompanyName = new JLabel("Company Name:");
+		lblCompanyName.setBounds(15, 85, 120, 20);
+		profileTab.add(lblCompanyName);
+		
+		txtCompanyName = new JTextField();
+		txtCompanyName.setBounds(145, 85, 120, 20);
+		profileTab.add(txtCompanyName);
+		txtCompanyName.setColumns(1);
+		txtCompanyName.setText(strCompanyName);
+		
+		JLabel lblAddress1 = new JLabel("Address:");
+		lblAddress1.setBounds(15, 110, 120, 20);
+		profileTab.add(lblAddress1);
+		
+		txtAddress1 = new JTextField();
+		txtAddress1.setBounds(145, 110, 120, 20);
+		profileTab.add(txtAddress1);
+		txtAddress1.setColumns(1);
+		txtAddress1.setText(strAddress1);
+		
+		JLabel lblAddress2 = new JLabel("Address (cont):");
+		lblAddress2.setBounds(15, 135, 120, 20);
+		profileTab.add(lblAddress2);
+		
+		txtAddress2 = new JTextField();
+		txtAddress2.setBounds(145, 135, 120, 20);
+		profileTab.add(txtAddress2);
+		txtAddress2.setColumns(1);
+		txtAddress2.setText(strAddress2);
+		
+		JLabel lblCity = new JLabel("City:");
+		lblCity.setBounds(15, 160, 120, 20);
+		profileTab.add(lblCity);
+		
+		txtCity = new JTextField();
+		txtCity.setBounds(145, 160, 120, 20);
+		profileTab.add(txtCity);
+		txtCity.setColumns(1);
+		txtCity.setText(strCity);
+		
+		JLabel lblState = new JLabel("State:");
+		lblState.setBounds(15, 185, 120, 20);
+		profileTab.add(lblState);
+		
+		txtState = new JTextField();
+		txtState.setBounds(145, 185, 120, 20);
+		profileTab.add(txtState);
+		txtState.setColumns(1);
+		txtState.setText(strState);
+		
+		JLabel lblZipCode = new JLabel("Zip Code:");
+		lblZipCode.setBounds(15, 210, 120, 20);
+		profileTab.add(lblZipCode);
+		
+		txtZipCode = new JTextField();
+		txtZipCode.setBounds(145, 210, 120, 20);
+		profileTab.add(txtZipCode);
+		txtZipCode.setColumns(1);
+		txtZipCode.setText(Integer.toString(intZipCode));
+		JLabel lblNewProfile = new JLabel("Updated Profile Settings:");
+		lblNewProfile.setBounds(360, 5, 200, 20);
+		profileTab.add(lblNewProfile);		
+		
+		JLabel lblLastNameUpdate = new JLabel("Last Name:");
+		lblLastNameUpdate.setBounds(345, 35, 120, 20);
+		profileTab.add(lblLastNameUpdate);
+		
+		txtLastName = new JTextField();
+		txtLastName.setBounds(475, 35, 120, 20);
+		profileTab.add(txtLastName);
+		txtLastName.setColumns(1);
+		
+		JLabel lblFirstNameUpdate = new JLabel("First Name:");
+		lblFirstNameUpdate.setBounds(345, 60, 120, 20);
+		profileTab.add(lblFirstNameUpdate);
+		
+		txtFirstName = new JTextField();
+		txtFirstName.setBounds(475, 60, 120, 20);
+		profileTab.add(txtFirstName);
+		txtFirstName.setColumns(1);
+		
+		JLabel lblCompanyNameUpdate = new JLabel("Company Name:");
+		lblCompanyNameUpdate.setBounds(345, 85, 120, 20);
+		profileTab.add(lblCompanyNameUpdate);
+		
+		txtCompanyName = new JTextField();
+		txtCompanyName.setBounds(475, 85, 120, 20);
+		profileTab.add(txtCompanyName);
+		txtCompanyName.setColumns(1);
+		
+		JLabel lblAddress1Update = new JLabel("Address:");
+		lblAddress1Update.setBounds(345, 110, 120, 20);
+		profileTab.add(lblAddress1Update);
+		
+		txtAddress1 = new JTextField();
+		txtAddress1.setBounds(475, 110, 120, 20);
+		profileTab.add(txtAddress1);
+		txtAddress1.setColumns(1);
+		
+		JLabel lblAddress2Update = new JLabel("Address (cont):");
+		lblAddress2Update.setBounds(345, 135, 120, 20);
+		profileTab.add(lblAddress2Update);
+		
+		txtAddress2 = new JTextField();
+		txtAddress2.setBounds(475, 135, 120, 20);
+		profileTab.add(txtAddress2);
+		txtAddress2.setColumns(1);
+		
+		JLabel lblCityUpdate = new JLabel("City:");
+		lblCityUpdate.setBounds(345, 160, 120, 20);
+		profileTab.add(lblCityUpdate);
+		
+		txtCity = new JTextField();
+		txtCity.setBounds(475, 160, 120, 20);
+		profileTab.add(txtCity);
+		txtCity.setColumns(1);
+		
+		JLabel lblStateUpdate = new JLabel("State:");
+		lblStateUpdate.setBounds(345, 185, 120, 20);
+		profileTab.add(lblStateUpdate);
+		
+		txtState = new JTextField();
+		txtState.setBounds(475, 185, 120, 20);
+		profileTab.add(txtState);
+		txtState.setColumns(1);
+		
+		JLabel lblZipCodeUpdate = new JLabel("Zip Code:");
+		lblZipCodeUpdate.setBounds(345, 210, 120, 20);
+		profileTab.add(lblZipCodeUpdate);
+		
+		txtZipCode = new JTextField();
+		txtZipCode.setBounds(475, 210, 120, 20);
+		profileTab.add(txtZipCode);
+		txtZipCode.setColumns(1);
+		
+		JButton btnSave = new JButton("Save Changes");
+		btnSave.setBounds(345, 247, 120, 23);
+		profileTab.add(btnSave);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clear();
+			}
+		});
+		btnCancel.setBounds(475, 247, 120, 23);
+		profileTab.add(btnCancel);
 		
 		JPanel calendarTab = new JPanel();
 		pageTabs.addTab("Calendar", null, calendarTab, null);
 		
 		JPanel openJobsTab = new JPanel();
 		pageTabs.addTab("Open Jobs", null, openJobsTab, null);
+	}
+	
+	public void clear(){
+//		txtLastNameUpdate.setText(null);
+//		txtFirstNameUpdate.setText(null);
+//		txtCompanyNameUpdate.setText(null);
+//		txtAddress1Update.setText(null);
+//		txtAddress2Update.setText(null);
+//		txtCityUpdate.setText(null);
+//		txtStateUpdate.setText(null);
+//		txtZipCodeUpdate.setText(null);
 	}
 }
