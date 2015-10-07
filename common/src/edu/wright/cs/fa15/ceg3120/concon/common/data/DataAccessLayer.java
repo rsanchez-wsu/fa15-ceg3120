1b5100a3233cc0b3d308eafacd2eca15bf3a3b0c
CMoorman <cmoorman@morleygp.com>
2015-10-07 13:29:19 -0400

    Adding in a DataAccessLayer class.

---
+++ common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/DataAccessLayer.java
@@ -0,0 +1,13 @@
+package edu.wright.cs.fa15.ceg3120.concon.common.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataAccessLayer {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DataAccessLayer.class);
+
+    public static void main(String[] args) {
+        LOG.trace("Starting Data Access Layer...");
+    }
+}
