02b90e7272e96cc12c634accbf34d6d6d70ac002
jsthomas45320 <thomas.611@wright.edu>
2015-10-08 11:05:18 -0400

    Updated import list

--- modules/contractor_client/src/edu/wright/cs/fa15/ceg3120/concon/client/contractor/ContractorClient.java
+++ modules/contractor_client/src/edu/wright/cs/fa15/ceg3120/concon/client/contractor/ContractorClient.java
@@ -26,11 +26,8 @@ import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DecimalFormat;
@@ -58,8 +55,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-import org.xml.sax.InputSource;
 
 public class ContractorClient extends JFrame implements ActionListener {
 
