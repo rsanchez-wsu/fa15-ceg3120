2f327995bfb33bcbfe0e7df1de0967497fc42e2b
Emily Novak <novak.17@wright.edu>
2015-10-09 22:05:55 -0400

    Added the invokeLater method to launch GUI in during build

--- modules/server/src/edu/wright/cs/fa15/ceg3120/concon/paysched/PaymentSchedulingWindow.java
+++ modules/server/src/edu/wright/cs/fa15/ceg3120/concon/paysched/PaymentSchedulingWindow.java
@@ -7,26 +7,32 @@
 package edu.wright.cs.fa15.ceg3120.concon.paysched;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
+//import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 /**
  *
  * @author Emily
  */
-public class PaymentSchedulingWindow {
+public class PaymentSchedulingWindow extends JFrame{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame paymentAndScheduling = new JFrame("Payment and Scheduling");
+    	SwingUtilities.invokeLater(new Runnable(){
+    		public void run(){
+    	    	   
+        TabbedFrame paymentAndScheduling = new TabbedFrame();
         paymentAndScheduling.setDefaultCloseOperation(EXIT_ON_CLOSE);
         paymentAndScheduling.setSize(475, 300);
-        TabbedFrame grid = new TabbedFrame();
-        grid.setVisible(true);
-        paymentAndScheduling.add(grid);
-        paymentAndScheduling.setVisible(true);
+        //TabbedFrame grid = new TabbedFrame();
+        //grid.setVisible(true);
+        //paymentAndScheduling.add(grid);
+        paymentAndScheduling.setVisible(true);}
+    	});
     }
-    
 }
+    
+
