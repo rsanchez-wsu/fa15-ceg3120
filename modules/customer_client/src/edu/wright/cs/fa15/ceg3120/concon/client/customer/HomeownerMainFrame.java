1eb864ee76e38410d5840ea09e04b43870c04db1
pquackenbush <quackenbush.2@wright.edu>
2015-10-13 22:45:09 -0400

    temp comments, added liscensing

--- modules/customer_client/src/edu/wright/cs/fa15/ceg3120/concon/client/customer/HomeownerMainFrame.java
+++ modules/customer_client/src/edu/wright/cs/fa15/ceg3120/concon/client/customer/HomeownerMainFrame.java
@@ -1,22 +1,51 @@
+/*
+ * Copyright (C) 2015
+ * 
+ * 
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package edu.wright.cs.fa15.ceg3120.concon.client.customer;
 
 import javax.swing.JFrame;
 
 
-
+/**
+ * temp.
+ *
+ */
 public class HomeownerMainFrame extends JFrame {
-private HomeownerMainPanel currentPanel;
+    private static final long serialVersionUID = 1L;
+    private HomeownerMainPanel currentPanel;
 	
-	public HomeownerMainFrame() 
-	{
+    /**
+     * temp.
+     */
+	public HomeownerMainFrame() {
 		currentPanel = new HomeownerMainPanel();
 		
 		setupFrame();
 		
 	}
 	
-	private void setupFrame()
-	{
+	/**
+	 * temp.
+	 */
+	private void setupFrame() {
 		this.setContentPane(currentPanel);
 		
 	}
--- modules/customer_client/src/edu/wright/cs/fa15/ceg3120/concon/client/customer/HomeownerMainPanel.java
+++ modules/customer_client/src/edu/wright/cs/fa15/ceg3120/concon/client/customer/HomeownerMainPanel.java
@@ -1,44 +1,69 @@
-package edu.wright.cs.fa15.ceg3120.concon.client.customer;
+/*
+ * Copyright (C) 2015
+ * 
+ * 
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-import javax.swing.*;
+package edu.wright.cs.fa15.ceg3120.concon.client.customer;
 
 import java.awt.Color;
 
+import javax.swing.JLayeredPane;
+import javax.swing.JTabbedPane;
 
-public class HomeownerMainPanel  extends JTabbedPane  {
-	private JLayeredPane Tab3;
-	
+/**
+ * temp.
+ *
+ */
+public class HomeownerMainPanel extends JTabbedPane {
+    private static final long serialVersionUID = 1L;
+    private JLayeredPane tab3;
 
-	
-	public HomeownerMainPanel() 
-	{
-		setBackground(Color.ORANGE);
-		
-		JLayeredPane Tab1 = new JLayeredPane();
-		addTab("Tab1", null, Tab1, null);
-		setEnabledAt(0, true);
-		setBackgroundAt(0, Color.WHITE);
-		
-		JLayeredPane Tab2 = new JLayeredPane();
-		addTab("Tab2", null, Tab2, null);
-		
-		Tab3 = new JLayeredPane();
-		addTab("Tab3", null, Tab3, null);
-		
-		JLayeredPane Tab4 = new JLayeredPane();
-		addTab("Tab4", null, Tab4, null);
-		
-		JLayeredPane Tab5 = new JLayeredPane();
-		addTab("Tab5", null, Tab5, null);
-		
-		
-		setupPanel();
-	}
+    /**
+     * Creates a new instance of <code>HomeownerMainPanel</code>.
+     */
+    public HomeownerMainPanel() {
+        setBackground(Color.ORANGE);
 
-	private void setupPanel()
-	{
-		
-	
-	
-	}
+        JLayeredPane tab1 = new JLayeredPane();
+        addTab("tab1", null, tab1, null);
+        setEnabledAt(0, true);
+        setBackgroundAt(0, Color.WHITE);
+
+        JLayeredPane tab2 = new JLayeredPane();
+        addTab("tab2", null, tab2, null);
+
+        tab3 = new JLayeredPane();
+        addTab("tab3", null, tab3, null);
+
+        JLayeredPane tab4 = new JLayeredPane();
+        addTab("tab4", null, tab4, null);
+
+        JLayeredPane tab5 = new JLayeredPane();
+        addTab("tab5", null, tab5, null);
+
+        setupPanel();
+    }
+
+    /**
+     * temp.
+     */
+    private void setupPanel() {
+
+    }
 }
