c1155e84438d47ca1caa5d9a9ed605f7ca3f0e96
Emily Novak <novak.17@wright.edu>
2015-10-16 16:17:31 -0400

    Included AddMoneyFrame and have it called in TabbedFrame.

--- modules/server/src/edu/wright/cs/fa15/ceg3120/concon/paysched/TabbedFrame.java
+++ modules/server/src/edu/wright/cs/fa15/ceg3120/concon/paysched/TabbedFrame.java
@@ -440,8 +440,29 @@ public class TabbedFrame extends JFrame{
     }//GEN-LAST:event_jButton1MouseClicked
 
     private void jbutton2MouseClicked(java.awt.event.MouseEvent evt) {
-    	//GEN-FIRST:event_jButton2MouseClicked
-        // TODO add your handling code here:
+    	 /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AddMoneyFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new AddMoneyFrame().setVisible(true);
+        });
     }//GEN-LAST:event_jButton2MouseClicked
 
     private void jbutton3MouseClicked(java.awt.event.MouseEvent evt) {
