6eb6e57250852c3adf5744abc91a7cb72472c76f
CMoorman <moorman.40@wright.edu>
2015-10-20 18:12:50 -0400

    Adding in a listener on a port and a loop that waits for input.

--- common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/DataAccessLayer.java
+++ common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/DataAccessLayer.java
@@ -21,22 +21,76 @@
 
 package edu.wright.cs.fa15.ceg3120.concon.common.data;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Javadoc needed.
- *
+ * Class for accessing database. Will be communicating with db through network stack.
  */
+@SuppressWarnings("unused")
 public class DataAccessLayer {
-
+	private String statement = "";
 	private static final Logger LOG = LoggerFactory.getLogger(DataAccessLayer.class);
+	
+	private static volatile ServerSocket serverSocket = null;
+	
+	Socket socket = null;
 
+	/*
+	 * Initializes
+	 */
+	private static void initializeSocketListener( int iPort ){
+		
+		// -- Check to see if the serverSocket is null or closed. If so, open it. -- CM
+		if( serverSocket == null ){
+			try{
+				
+				serverSocket = new ServerSocket( iPort );
+				
+				// -- Create our thread that will be listening on the specified port. -- CM
+				Thread socketListenerThread = new Thread(new Runnable() {
+				    public void run() {
+				    	try{
+				    		// -- Start accepting input.
+							Socket clientSocket = serverSocket.accept();
+							
+							// -- Runs a continuous loop that will grab input on the socket. -- CM
+							for(;;){
+								if( clientSocket.isConnected() 
+										&& clientSocket.getInputStream().toString().length() > 0 ) {
+									// -- If we reached here, the inputstream had some value in it. Handle it.
+									
+								}
+							}// -- End of continuous loop
+							
+						}catch( Exception e ){
+							// -- Exception caught, what happened and what do we do?
+							LOG.debug( e.toString() );
+						}
+				   }}); // -- End of Thread()
+				
+				// -- Start the thread.
+				socketListenerThread.start();
+				
+			}catch( Exception e ){
+				// -- Exception caught, what happened and what do we do? -- CM
+				LOG.debug( e.toString() );
+			}	
+		}
+	}
+	
 	/**
-	 * Javadoc needed.
-	 *
+	 * Main method, starts the process of listening
 	 */
 	public static void main(String[] args) {
 		LOG.trace("Starting Data Access Layer...");
+		
+		int iPort = 4001; 
+		
+		initializeSocketListener(iPort);
+		
 	}
 }
