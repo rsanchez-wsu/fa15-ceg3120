cbdcbe7fe7f0d8860f4cfd293232dd9ec09b2a70
CMoorman <moorman.40@wright.edu>
2015-10-22 14:21:58 -0400

    Adding in some threading to handle database requests.  This commit adds a general function that takes an object and request type.  The function then creates a new thread based off of what the request type is.

--- common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/DataAccessLayer.java
+++ common/src/edu/wright/cs/fa15/ceg3120/concon/common/data/DataAccessLayer.java
@@ -36,9 +36,185 @@ public class DataAccessLayer {
 	private String statement = "";
 	private static final Logger LOG = LoggerFactory.getLogger( DataAccessLayer.class );
 	
+	/*
+	 * Final static string that will be used to help identify the objects coming into the
+	 * data access layer.  The object that is coming in must have a class type of the following:
+	 * */
+	private final static String sContractorAccount = "ContractorAccount";
+	private final static String sContractorReview  = "ContractorReview";
+	private final static String sHomeownerAccount  = "HomeownerAccount";
+	private final static String sHomeownerReview   = "HomeownerReview";
+	private final static String sJobRequest		   = "JobRequest";
+	private final static String sReview			   = "Review";
+	private final static String sServerAdminAccount= "ServerAdminAccount";
+	private final static String sTag			   = "Tag";
+	private final static String sUserAccount       = "UserAccount";
+	
+	/**
+	 * Take in a generic object and a request type.  This function will act as the distributor
+	 * of work for the data access layer.  The work will be passed out based on the request type.
+	 * 
+	 * Each new request will create it's own thread which will allows multiple requests to be handled.
+	 * 
+	 * Inputs are set to final as they are not to be modified on their way to the DB.
+	 * 
+	 * @param final inputData
+	 * @param final reqType
+	 * @return
+	 */
+	public static void sendDatabaseRequest(final Object inputData, final RequestType reqType){
+		
+		LOG.trace("-- DataAccessLayer.sendDatabaseRequest. " + 
+				inputData.getClass().toString() + "; " + 
+				"Req: " + reqType.toString() );
+		
+		if( inputData != null && reqType != null ){
+			switch(reqType){
+			case CREATE:
+			
+				Thread newCreateThread = new Thread() {
+				    public void run() {
+				        try {
+				        	processCreateRequest(inputData);
+				        } catch( Exception e ) {
+				        	logFailedThreadRequest( e, reqType);
+				        }
+				    }
+				};
+				newCreateThread.start();
+				break;
+				
+			case DELETE:
+				Thread newDeleteThread = new Thread() {
+				    public void run() {
+				        try {
+				        	processDeleteRequest(inputData);
+				        } catch( Exception e ) {
+				        	logFailedThreadRequest( e, reqType);
+				        }
+				    }
+				};
+				newDeleteThread.start();
+				break;
+				
+			case READ:
+				
+				Thread newReadThread = new Thread() {
+				    public void run() {
+				        try {
+				        	processReadRequest(inputData);
+				        } catch( Exception e ) {
+				        	logFailedThreadRequest( e, reqType);
+				        }
+				    }
+				};
+				newReadThread.start();
+				break;
+				
+			case UPDATE:
+				
+				Thread newUpdateThread = new Thread() {
+				    public void run() {
+				        try {
+				        	processUpdateRequest(inputData);
+				        } catch( Exception e ) {
+				        	logFailedThreadRequest( e, reqType);
+				        }
+				    }
+				};
+				newUpdateThread.start();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param inputData
+	 * @return
+	 */
+	private static boolean processCreateRequest(Object inputData){
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param inputData
+	 * @return
+	 */
+	private static boolean processReadRequest(Object inputData){
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param inputData
+	 * @return
+	 */
+	private static boolean processUpdateRequest(Object inputData){
+		return false;
+	}
+	/**
+	 * 
+	 * @param inputData
+	 * @return
+	 */
+	private static boolean processDeleteRequest(Object inputData){
+		return false;
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @param inputData
+	 * @return
+	 */
+	/*public static boolean commitDataObject(Object inputData){
+		
+		if( inputData != null ){
+			if( inputData.getClass().equals(ContractorAccount.class) ){
+				
+			}
+			else if( inputData.getClass().equals(ContractorReview.class) ){
+				
+			}
+			else if( inputData.getClass().equals(HomeownerAccount.class) ){
+				
+			}
+			else if( inputData.getClass().equals(HomeownerReview.class) ){
+				
+			}
+			else if( inputData.getClass().equals(JobRequest.class) ){
+				
+			}
+			else if( inputData.getClass().equals(Review.class) ){
+				
+			}
+			else if( inputData.getClass().equals(ServerAdminAccount.class) ){
+				
+			}
+			else if( inputData.getClass().equals(Tag.class) ){
+				
+			}
+			else{
+				
+			}
+		}
+		
+		
+		// -- Return false, we did not commit the object.
+		return false;
+	}*/
+	
+	private static void logFailedThreadRequest( Exception e, RequestType reqType ){
+		LOG.trace("FAILED: -- DataAccessLayer. Failed to start thread for " + 
+				reqType.toString() + " request.\n" + " -- Error: " + e.toString() );
+	}
+	
 	public static void main(String[] args) {
 		LOG.trace("Starting Data Access Layer...");
-		
-		int iPort = 4001; 
 	}
 }
