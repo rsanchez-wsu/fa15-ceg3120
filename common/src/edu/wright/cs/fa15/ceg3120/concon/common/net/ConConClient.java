73d83baad0e0b177eadaec655ee7d787c1a06fdb
Emily Novak <novak.17@wright.edu>
2015-11-19 21:26:35 -0500

    Commented out an unused import statement in ConConClient

--- common/src/edu/wright/cs/fa15/ceg3120/concon/common/net/ConConClient.java
+++ common/src/edu/wright/cs/fa15/ceg3120/concon/common/net/ConConClient.java
@@ -27,7 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.Closeable;
+//import java.io.Closeable;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
